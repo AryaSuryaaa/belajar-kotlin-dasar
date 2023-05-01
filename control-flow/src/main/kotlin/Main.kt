c0731c45232b6f42d1114c041818459c9a203d66
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-01 17:57:04 +0700

    Belajar Bab Control Flow, materi Enumeration (Update)

--- control-flow/src/main/kotlin/Main.kt
+++ control-flow/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.awt.Color
+import kotlin.random.Random
 
 fun main(args: Array<String>) {
 
@@ -34,6 +35,59 @@ fun main(args: Array<String>) {
         Colors.BLUE -> println("Color is BLUE")
     }
 
+    // Expression
+    // expression adalah statement yang dapat mengembalikan nilai dan bisa kita simpan ke dalam sebuah variabel seperti
+    // Statement
+    // tidak mengembalikan nilai (variabel value1&2)
+    val value1 = 10
+    val value2 = 20
+
+    sum(value1, value2)
+
+    // When Expression
+    val stringOfValue = when(value1) {
+        6 -> {
+            println("six")
+            "value is 6"
+        }
+        7 -> {
+            println("sever")
+            "value is 7"
+        }
+
+
+        else -> {
+            println("undefined")
+            "value cannot be reached"
+        }
+    }
+    println(stringOfValue)
+
+    // memeriksa instance dengan tipe tertentu
+    // menggunakan is / !is
+    val anyType: Any = 100L
+    when(anyType) {
+        is Long -> println("the value has a Long type")
+        is String -> println("the value has a String type")
+        else -> println("undefined")
+    }
+
+    // when untuk range
+    val nilai = 27
+    val range = 10..50
+
+    when(nilai) {
+        in range -> println("values is in the range")
+        !in range -> println("value is outside the range")
+        else -> println("value out of range")
+    }
+
+    val registerNumber = when(val regis = getRegisterNumber()){
+        in 1..50 -> 50 * regis
+        in 51..100 -> 100 * regis
+        else -> regis
+    }
+    println(registerNumber)
 }
 
 enum class Colors(val value: Int) {
@@ -41,3 +95,7 @@ enum class Colors(val value: Int) {
     GREEN(0x00FF00),
     BLUE(0x0000FF)
 }
+
+fun sum(value1: Int, value2: Int) = value1 + value2
+
+fun getRegisterNumber() = Random.nextInt(100)
